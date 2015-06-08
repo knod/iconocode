b985949ef97ec5b2ac709e58002c7b597539d7da
knod <knodding@gmail.com>
2015-06-08 12:08:03 -0400

    Nodes ranked correctly, I believe. Not yet rearranged.

--- js2/search-images.js
+++ js2/search-images.js
@@ -2,27 +2,30 @@
 
 Trying to implement fuzzy search functionality with
 multiple lists for images.
+
+TODO:
+- Hide images that have no matching terms
+- Rank images by how closely they match the query
+- Re-order images to appear by rank of match
 */
 
 'use strict'
 
 
-// Take the form of [{ fileName: '', searchTerms: [], folderPath: '' }, ...]
-var imageObjArray 		= adder.defaultImages;
-
-var fuzzySearcher 		= new FuzzySearcher();
-
-
 adder.updateChoiceList 	= function ( choiceNode, query ) {
 /*
 
-Gets the matching terms for this choice and shows just them.
-Doesn't get rid of search terms.
+Runs the search for matches on each choiceNode. Shows just
+matching terms. Doesn't get rid of search terms.
 */
-	var terms 	= $(choiceNode).data('terms');
+	var terms 			= $(choiceNode).data('terms');
+	var fuzzySearcher 	= new FuzzySearcher();
 	
-	// Search within each of those and do stuff with the results
+	// Get match data and store it in the node
 	var matchData 	= fuzzySearcher.toNode( terms, query );
+	$(choiceNode).data('matchData', matchData);
+
+	// Re-make list of terms with styled list elements
 	var list 		= $(choiceNode).parent().find('ol')[0];
 	$(list).empty();
 	list.appendChild( matchData.node );
@@ -36,13 +39,40 @@ adder.updateChoiceLists = function ( choiceGrid, query ) {
 
 Updates the visible search terms for all choices.
 */
+	var choiceNodes = [];
+
 	for ( var rowi = 0; rowi < choiceGrid.length; rowi++ ) {
 		for ( var coli = 0; coli < choiceGrid[rowi].length; coli++ ) {
 			// Change the visible search terms for that image
 			var choiceNode = choiceGrid[ rowi ][ coli ];
 			adder.updateChoiceList( choiceNode, query );
+			// List to be ranked
+			choiceNodes.push(choiceNode); console.log(choiceNode)
 		}
 	}
+
+	var matchComparator = function ( node1, node2 ) {
+	/* This is in here because in here is where the test properties are created */
+		// !!!: TODO: This is terrible. I have to dig way down deep
+		var match2 = $(node2).data('matchData').matchesData[0];
+		var match1 = $(node1).data('matchData').matchesData[0];
+
+		var diff = match2.score - match1.score;
+
+		// If the scores are the same
+		if ( match1.score === match2.score ) {
+			// Add the number of letters before the first match
+			match2.altScore = match2.score - match2.term.length;
+			match1.altScore = match1.score - match1.term.length;
+
+			diff = match2.altScore - match1.altScore;
+		}
+
+		return diff;
+	};  // End matchComparator()
+
+	console.log(choiceNodes.sort( matchComparator ));
+	
 
 	return choiceGrid;
 };  // End adder.updateChoiceLists()
