24f0986a5205ad7adaafc2cbdf603591793e835a
knod <knodding@gmail.com>
2015-06-18 18:56:59 -0400

    Grid object creation added. Test at bottom of page.

--- js2/Grid.js
+++ js2/Grid.js
@@ -3,3 +3,111 @@
 * Handles a grid of choices/options that
 * are selectable and choosable
 */
+
+
+adder.Grid = function ( pickerName, maxCols, choiceNodes ) {
+/* ( str, int, [Nodes] ) -> {}
+* 
+* 
+*/
+
+	var thisGrid = {};
+
+	thisGrid.choiceNodes 	= choiceNodes;
+	thisGrid.rowNodes 		= [];
+
+
+	// ========================
+	// CREATION
+	// ========================
+	thisGrid.setRow = function ( pickerName, rowNum, rowNodes ) {
+	/* ( int, [Nodes], Node ) -> Node
+	* 
+	* Adds a row of choice nodes to the Picker node
+	*/
+		var rowNode 		= document.createElement( 'div' );
+
+		var parentNode = document.getElementById( 'icd_' + pickerName + '_picker' )
+		parentNode.appendChild( rowNode );
+
+		rowNode.className 	= pickerName + '-picker-row';
+		rowNode.id 			= pickerName + '_choice_row' + rowNum;
+
+		// // This is what the grid will actully use to access selections or navigate?
+		// // Not sure this is needed
+		// var rowArray 		= [];
+
+		// Give the choice node its id and position data
+		for ( var nodei = 0; nodei < rowNodes.length; nodei++ ) {
+			var node = rowNodes[ nodei ];
+			console.log(node)
+
+			// If it's undefined it means we've run out of nodes (bottom row)
+			if ( node !== undefined ) {
+				rowNode.appendChild( node );
+
+				node.id = rowNode.id + '_col' + nodei;
+				$(node).data('row', rowNum);  // TODO: check if this is used
+				$(node).data('col', nodei);  // TODO: check if this is used
+			}
+
+		}
+
+		return rowNode;
+	};  // End thisGrid.addRow()
+
+
+	thisGrid.set 	= function ( pickerName, maxCols, choiceNodes ) {
+	/**/
+		var rowNodes = [];
+
+		// Get the right number of rows for the given number of choices, each time
+		// Takes into account having a non-evenly divided number
+		var numRows = Math.ceil( choiceNodes.length / maxCols )
+
+		for ( var rowi = 0; rowi < numRows; rowi++ ) {
+
+			// Get the relevant choice nodes for this row.
+			var startIndx 	= maxCols * rowi,
+			// Use maxCols because .slice() doesn't include the last item
+				endIndx		= startIndx + maxCols;
+			var rowNodes 	= choiceNodes.slice( startIndx, endIndx );
+
+			// Add a row to the DOM, then add the row's node to the list
+			var rowNode = thisGrid.setRow( pickerName, rowi, rowNodes );
+			rowNodes.push( rowNode );
+
+		}
+
+		thisGrid.rowNodes = rowNodes;
+		return rowNodes;
+	};  // End thisGrid.set()
+
+
+	thisGrid.set( pickerName, maxCols, choiceNodes );
+
+	return thisGrid;
+};  // End adder.Grid {}
+
+
+// *************************************************
+// Testing
+// *************************************************
+var pickerName = 'images';
+var maxCols = 5;
+var imageChoiceNodesTest = [];
+
+var allImageObjs = adder.defaultImages
+
+for ( var imgi = 0; imgi < allImageObjs.length; imgi++ ) {
+	var imgObj = allImageObjs[ imgi ]
+	var parent = document.createDocumentFragment();
+
+	var imgChoice 	= new adder.ImgChoice( imgObj, parent );
+	imageChoiceNodesTest.push( imgChoice.node );
+}
+
+// Test in console:
+// var myGrid = adder.Grid( pickerName, maxCols, imageChoiceNodesTest );
+
+
---
+++ js2/ImageChoice2.js
@@ -0,0 +1,95 @@
+/* ICD-Image.js
+
+Builds an image node correctly for insertion into an icon
+*/
+
+'use strict';
+
+adder.ImgChoice = function ( imgObj, parentNode ) {
+
+	var imgChoice = {};
+
+	imgChoice.node = null;
+
+	imgChoice.addImgTagList 	= function ( imgObj, parentNode ) {
+	/* ( {}, Node ) -> other Node
+
+	Adds the list that all the search terms will be added to
+	It's a list because later we'll show query matches there and
+	they'll be a list too.
+	*/
+		var tagContainer 		= document.createElement('div');
+		parentNode.appendChild( tagContainer );
+		tagContainer.className 	= 'image-tag-list-container';
+
+		var list 		= document.createElement('ol');
+		tagContainer.appendChild( list );
+		list.className 	= 'image-tag-list';
+		list.style.position = 'absolute';
+
+		var listItem 	= document.createElement('li');
+		list.appendChild( listItem );
+		listItem.className = 'image-tag';
+
+		var firstTerm 	= document.createTextNode( imgObj.searchTerms[0] );
+		listItem.appendChild( firstTerm );
+
+		return list;
+	};  // End imgChoice.addImgTagList()
+
+
+	imgChoice.addImage 			= function ( imgFilePath, parentNode ) {
+	/*
+
+	Just create, return, and add an image node with the specified path
+	*/
+		var imgNode 	= document.createElement('img');
+		parentNode.appendChild( imgNode );
+		imgNode.src 	= imgFilePath;
+
+		return imgNode;
+	};  // End imgChoice.addImage()
+
+
+	imgChoice.addChoice 	= function ( imgObj, parentNode ) {
+	/* ( str, Node ) -> new Node
+
+	Maybe way to do image size to maximize image http://jsfiddle.net/0bmws0me/1/
+	(from TheP... something), but maybe we want the images to be their proper size?
+	*/
+		// --- CONTAINER --- \\
+		var imgContainer 		= document.createElement('div');
+		parentNode.appendChild( imgContainer );
+		imgContainer.className 	= 'image-choice-container';
+
+		// --- IMAGE NODE --- \\
+		var filePath 	= imgObj.folderPath + imgObj.fileName;
+		var imgNode 	= imgChoice.addImage( filePath, imgContainer );
+		$(imgNode).addClass('image-choice');
+		$(imgNode).data('terms', imgObj.searchTerms);
+
+		// Allows image to recieve focus (not a usual thing for images)
+		imgNode.tabIndex = '0';
+		// No id?
+		// Will use src of clicked image to add correct image
+		// No label?
+
+		// TODO: add when clicked
+		imgNode.addEventListener( 'keydown', function ( evnt ) {
+			adder.imgKeyHandler( evnt );
+		});
+
+		// --- TERM LIST --- \\
+		imgChoice.addImgTagList( imgObj, imgContainer )
+
+		imgChoice.node = imgContainer;
+		return imgNode;
+	}  // End adder.addChoice()
+
+
+
+	imgChoice.addChoice( imgObj, parentNode );
+
+	return imgChoice;
+};  // End adder.ImgChoice {}
+
