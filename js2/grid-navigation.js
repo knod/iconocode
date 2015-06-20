2648341153a449614276f79c54ebc906257494f5
knod <knodding@gmail.com>
2015-06-20 09:12:07 -0400

    Changes how tab activation is run. Better? Who can tell.

---
+++ js2/grid-navigation.js
@@ -0,0 +1,15 @@
+/* grid-navigation.js
+
+Navigating any grid using keyboard input
+Somehow based on mode?
+*/
+
+adder.setupGridNavigation = function ( thisGrid ) {
+
+	
+
+
+
+
+	return thisGrid;
+};  // End adder.setupGridNavigation()
