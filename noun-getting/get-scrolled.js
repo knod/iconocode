97bca55ba9ba5e99e8a4da87c60e12dc2cc4eeb2
knod <knodding@gmail.com>
2015-07-16 14:25:15 -0400

    Inits intergration of outside source of nouns

---
+++ noun-getting/get-scrolled.js
@@ -0,0 +1,14 @@
+// get-scrolled.js
+
+
+// Get all the hrefs from the main page
+
+
+// Get images with their href's for tags
+
+// Get all hrefs
+
+// Scrape those tags and download the file
+
+
+// Get 
